f47b077315b6c5407596d6f1c929055e1741a8aa
CEShen <brian1020306@gmail.com>
2022-11-24 13:43:05 +0800

    update: scrap_subtitle

--- scrap_subtitile.py
+++ scrap_subtitile.py
@@ -10,14 +10,14 @@ import json
 「User-Agent」算是一個讓伺服器最簡易判斷送出請求的人是誰，是電腦瀏覽器？是手機瀏覽器？還是Google爬蟲？
 不過就像我們上面程式操作的，它也很容易自己去更改。
 """
+sn = 31951
 h = {
 'Content-Type':'application/x-www-form-urlencoded;charset=utf-8',
 'origin':'https://ani.gamer.com.tw',
 'authority':'ani.gamer.com.tw',
 'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.83 Safari/537.36'
 }
-url = "https://ani.gamer.com.tw/animeVideo.php?sn=32004"
-data = {'sn':'32004'}
+data = {'sn':f'{sn}'}
 re = requests.post('https://ani.gamer.com.tw/ajax/danmuGet.php', data=data, headers=h)
 
 if re.status_code == 200:
@@ -26,7 +26,12 @@ else:
     print(f"fail request QAQ, status code = {re.status_code}")
 
 anime_soup = BeautifulSoup(re.text, "html.parser")
-# print(anime_soup.findAll("li"))
-j = json.loads(re.text)
-for obj in j:
-    print(obj['text'])
+danmu_json = json.loads(re.text) #keys: ['text', 'color', 'size', 'position', 'time', 'sn', 'userid']
+class Danmu:
+    def __init__(self, sn:int, text:str, userid:str) -> None:
+        self._sn = sn
+        self.text = text
+        self.userid = userid
+        return
+danmu_obj_list = [Danmu(sn, danmu["text"], danmu["userid"]) if "簽" not in danmu["text"] else None for danmu in danmu_json]
+print(danmu_obj_list)
